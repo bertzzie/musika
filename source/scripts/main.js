1ab15c374c54018afe5dc282653260026b05a39b
Alex Xandra Albert Sim <bertzzie@gmail.com>
2013-12-27 11:57:24 +0700

    Refactorred playlist class.
    We add a new tracklist class, and use it for storing
    tracks in playlist. This class will be useful next,
    for storing more information about the added class.
    For example, if we want to store the file path, we
    can just add a new property in this class, and we
    won't clutter the playlist class.

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -18,71 +18,80 @@
         this.classList.remove(name);
     });
     
-    var playlist = function (spec) {
+    var tracklist = function (spec) {
         var el = document.getElementById(spec.id),
-            tracks = document.getElementById(spec.trackId),
-            removeDefaultText = function () {
-                var length = el.childNodes.length,
-                    children = el.childNodes,
-                    removedChild = [],
-                    i;
-                
-                for (i = 0; i < length; i += 1) {
-                    if (children[i].nodeName === "#text" &&
-                            children[i].textContent.trim() === "Drop your music here.") {
-                        removedChild.push(children[i]);
-                    }
-                }
-                
-                removedChild.forEach(function (child) {
-                    el.removeChild(child);
-                });
-            },
-            dropHandler = function (evt) {
-                var files = evt.dataTransfer.files,
-                    length = evt.dataTransfer.files.length,
-                    newMusic,
-                    i;
-                
-                evt.stopPropagation();
-                evt.preventDefault();
-                
-                removeDefaultText();
-                el.addClass(spec.filledClass);
-                
-                for (i = 0; i < length; i += 1) {
-                    newMusic = document.createElement("li");
-                    newMusic.textContent = files[i].name;
-                    tracks.appendChild(newMusic);
-                }
-                
-                el.removeClass(spec.overClass);
-            },
-            dragOverHandler = function (evt) {
-                evt.stopPropagation();
-                evt.preventDefault();
-                evt.dataTransfer.dropEffect = "copy";
-            },
-            dragEnterHandler = function () {
-                el.addClass(spec.overClass);
-            },
-            dragLeaveHandler = function () {
-                el.removeClass(spec.overClass);
-            },
             that = {};
         
-        // that only have initation method for now, but we'll 
-        // add more functionality later. 
-        // E.g.: tracking current music.
-        that.init = function () {
-            el.addEventListener("dragover", dragOverHandler, false);
-            el.addEventListener("dragenter", dragEnterHandler, false);
-            el.addEventListener("dragleave", dragLeaveHandler, false);
-            el.addEventListener("drop", dropHandler, false);
+        that.addTrack = function (name) {
+            var newTrack = document.createElement("li");
+            newTrack.textContent = name;
+            el.appendChild(newTrack);
         };
         
         return that;
-    };
+    },
+        playlist = function (spec) {
+            var el = document.getElementById(spec.id),
+                tracks = tracklist({id: spec.trackId}),
+                removeDefaultText = function () {
+                    var length = el.childNodes.length,
+                        children = el.childNodes,
+                        removedChild = [],
+                        i;
+                    
+                    for (i = 0; i < length; i += 1) {
+                        if (children[i].nodeName === "#text" &&
+                                children[i].textContent.trim() === "Drop your music here.") {
+                            removedChild.push(children[i]);
+                        }
+                    }
+                    
+                    removedChild.forEach(function (child) {
+                        el.removeChild(child);
+                    });
+                },
+                dropHandler = function (evt) {
+                    var files = evt.dataTransfer.files,
+                        length = evt.dataTransfer.files.length,
+                        i;
+                    
+                    evt.stopPropagation();
+                    evt.preventDefault();
+                    
+                    removeDefaultText();
+                    el.addClass(spec.filledClass);
+                    
+                    for (i = 0; i < length; i += 1) {
+                        tracks.addTrack(files[i].name);
+                    }
+                    
+                    el.removeClass(spec.overClass);
+                },
+                dragOverHandler = function (evt) {
+                    evt.stopPropagation();
+                    evt.preventDefault();
+                    evt.dataTransfer.dropEffect = "copy";
+                },
+                dragEnterHandler = function () {
+                    el.addClass(spec.overClass);
+                },
+                dragLeaveHandler = function () {
+                    el.removeClass(spec.overClass);
+                },
+                that = {};
+            
+            // that only have initation method for now, but we'll 
+            // add more functionality later. 
+            // E.g.: tracking current music.
+            that.init = function () {
+                el.addEventListener("dragover", dragOverHandler, false);
+                el.addEventListener("dragenter", dragEnterHandler, false);
+                el.addEventListener("dragleave", dragLeaveHandler, false);
+                el.addEventListener("drop", dropHandler, false);
+            };
+            
+            return that;
+        };
     
     document.addEventListener("DOMContentLoaded", function () {
         var pl = playlist({
